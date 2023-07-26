b83e33be2dcd727f0811d96fba55dc18e2afd56d
Abdellah El bekkali <abekkali451@gmail.com>
2023-07-26 10:26:33 +0100

    init library bits/stdc++.h

---
+++ bits/stdc++.h
@@ -0,0 +1,37 @@
+#ifndef __STDC++_H__
+#define __STDC++_H__
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#endif
