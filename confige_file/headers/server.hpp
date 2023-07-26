0c2bebbc0a3c9bc2bf3aaf3bdc6c05b37b596890
Abdellah El bekkali <abekkali451@gmail.com>
2023-07-26 12:15:02 +0100

    add get and set to the server class

--- confige_file/headers/server.hpp
+++ confige_file/headers/server.hpp
@@ -8,7 +8,7 @@ class __server {
     private:
         std::string                 server_name;
         std::string                 host;
-        std::vector<unsigned short> listen;
+        std::vector<unsigned short> port;
         std::map<int, std::string>  error_page;
         size_t                      client_max_body_size;
         std::vector <__location>    location;
@@ -18,6 +18,21 @@ class __server {
         __server(__server const & server);
         __server &operator=(__server const & server);
 
+        std::string                 get_server_name();
+        std::string                 get_host();
+        std::vector<unsigned short> get_ports();
+        std::map<int, std::string>  get_error_pages();
+        size_t                      get_client_max_body_size();
+        std::vector<__location>     get_locations();
+
+        void    set_server_name();
+        void    set_host();
+        void    set_ports();
+        void    set_error_pages();
+        void    set_client_max_body_size();
+        void    set_locations();
+        
+        void    _set(); // set all server infos
 };
 
 #endif
